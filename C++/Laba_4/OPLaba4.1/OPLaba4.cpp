b933cb25a2250796f53d94519cf2a4c822a09e3c
Bulbazavrik <SergiyGryffindor007@gmail.com>
2022-07-30 14:54:25 +0200

    aggiunta_4_lab

---
+++ C++/Laba_4/OPLaba4.1/OPLaba4.cpp
@@ -0,0 +1,62 @@
+﻿/* Вариант 3. Часть 1
+* Дана последовательность натуральных чисел {aj}j=1...n (n<=10000).
+* Если в последовательности есть хотя бы одно число, сумма цифр которого равна 14, , упорядочить 
+последовательность по невозрастанию последней цифры числа. 
+*/ 
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <windows.h>
+#define N 1000
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int mas[N], n, i, j;
+	int sum, x;
+	cout << " Введите количество чисел: " << "\n";
+	cin >> n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		cout << " Введите число: " << "\n";
+		cin >> mas[i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		sum = 0;
+		x = mas[i];
+		while (x > 0)
+		{
+			sum += (mas[i] % 10);
+			x /= 10;
+		}
+		if (sum == 14) 
+		{
+			for (i = 0; i < n ; i++)
+			{
+				for (j = i + 1; j < n; j++)
+					if (mas[i] < mas[j])
+					{
+						swap(mas[i], mas[j]);
+						
+					}
+			}
+			
+			for (i = 0; i < n; i++) {
+				cout << mas[i] << " ";
+			}
+			return 2;
+		}
+
+		else {
+			cout << "Нет чисел, сумма которых равна 14" << "\n";
+			return 1;
+		}
+	}
+	return 0;
+}
+
---
+++ C++/Laba_4/OPLaba4.3/OPLaba4.3.cpp
@@ -0,0 +1,51 @@
+﻿/*Вариант 3. Часть 3.
+* Дана целочисленная матрица {Aij}i=1..n,j=1..m (n,m <=100). Найти столбец
+* содержащий наименьший элемент матрицы и заменить все отрицательные элементы
+* этого столбца числом 0.
+*/
+
+#include<fstream>
+#include <iostream>
+#define N 100
+#define M 100
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int matrix[N][M];
+	int n, m, m1, stolb = 0;
+	ifstream in("inc/input.txt");
+	in >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			in >> matrix[i][j];
+		}
+	}
+	// поиск мин. элемента
+	int min = matrix[0][0];
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (min > matrix[i][j])
+			{
+				min = matrix[i][j];
+				stolb = j+1;
+				m1 = j;
+			}
+		}
+	}
+
+	//Вывод
+	ofstream out("inc/output.txt");
+	for (int i = 0; i < n; i++) {
+		if (matrix[i][m1] < 0) {
+			matrix[i][m1] = 0;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			out << matrix[i][j] << " ";
+		}
+	}
+	out << "Столбец с мин. значением = " << stolb<< "\n";
+	return 0;
+}
