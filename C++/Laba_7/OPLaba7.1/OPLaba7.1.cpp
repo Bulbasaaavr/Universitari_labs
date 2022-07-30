5ccfe4a959c5796b546c770eb67efecdf546f02a
Bulbazavrik <SergiyGryffindor007@gmail.com>
2022-07-30 20:06:45 +0200

    aggiunta_7_lab

---
+++ C++/Laba_7/OPLaba7.1/OPLaba7.1.cpp
@@ -0,0 +1,71 @@
+﻿/* Вариант 3. Часть 2
+* Ввести последовательность натуральных чисел {Aj}j=1...n (n<=10000).
+* Упорядочить последовательность по неубыванию первой цифры числа, числа с одинаковыми первыми цифрами дополнительно
+упорядочить по неубыванию наибольшей цифры числа, числа с одинаковыми первыми цифрами и одинаковыми наибольшими цифрами
+дополнительно упорядочить по неубыванию самого числа.
+Реализовать через динамические массивы.
+*/
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <windows.h>
+#define N 1000
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n, kol;
+	int* mas = new int [N];
+	int* firnum = new int[N];
+	int* maxnum = new int[N];
+	cout << " Введите количество чисел: " << "\n";
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << " Введите числа: " << "\n";
+		cin >> mas[i];
+		kol = mas[i];
+		while (kol > 10)
+		{
+			firnum[i] = kol / 10;
+			kol /= 10;
+		}
+		maxnum[i] = -1;
+		kol = mas[i];
+		while (kol > 0)
+		{
+			if (kol % 10 > maxnum[i])
+			{
+				maxnum[i] = kol % 10;
+
+			}
+			kol /= 10;
+		}
+	}
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if ((firnum[i] > firnum[j]) ||
+				((firnum[i] == firnum[j]) && (maxnum[i] > maxnum[j])) ||
+				((firnum[i] == firnum[j]) && (maxnum[i] == maxnum[j]) && (mas[i] > mas[j])))
+			{
+				swap(mas[i], mas[j]);
+				swap(firnum[i], firnum[j]);
+				swap(maxnum[i], maxnum[j]);
+			}
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << mas[i] << " ";
+	}
+		delete[] mas;
+		delete[] firnum;
+		delete[] maxnum;
+
+	return 0;
+}
---
+++ C++/Laba_7/OPLaba7.2/OPLaba7.2.cpp
@@ -0,0 +1,60 @@
+﻿/*Вариант 3. Часть 3.
+* Дана целочисленная матрица {Aij}i=1..n,j=1..m (n,m <=100). Найти столбец
+* содержащий наименьший элемент матрицы и заменить все отрицательные элементы
+* этого столбца числом 0.
+* Реализовать через динамические массивы
+*/
+
+#include<fstream>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n, m, m1, stolb;
+	ifstream in("input.txt");
+	in >> n >> m;
+	int** matrix = new int* [n];
+	for (int i = 0; i < n; i++)
+	{
+		matrix[i] = new int[m];
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			in >> matrix[i][j];
+		}
+	}
+	// поиск мин. элемента
+	int min = matrix[0][0];
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (min > matrix[i][j])
+			{
+				min = matrix[i][j];
+				stolb = j + 1;
+				m1 = j;
+			}
+		}
+	}
+
+	//Вывод
+	ofstream out("output.txt");
+	for (int i = 0; i < n; i++) {
+		if (matrix[i][m1] < 0) {
+			matrix[i][m1] = 0;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			out << matrix[i][j] << " ";
+		}
+	}
+	out << "Столбец с мин. значением = " << stolb << "\n";
+	
+	for (int i = 0; i < m; i++) {
+		delete[] matrix[i];
+	}
+	delete[] matrix;
+	return 0;
+}
